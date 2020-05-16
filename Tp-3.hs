03cc4e708feeb804a9dcb5229ea2159fb5ab185d
IanStratico <straticoian@gmail.com>
2020-05-16 20:58:17 -0300

    v1.2

--- Tp-3.hs
+++ Tp-3.hs
@@ -2,16 +2,17 @@ import Data.List
 import Text.Show.Functions()
 
 data  Participante = Participante { 
-nombre               :: String,
+nombre               :: Nombre,
 cantidadDeDinero     :: Int,
 tacticaDeJuego       :: Tactica,
 propiedadesCompradas :: [Propiedad],
 accionesDelJuego     :: [Accion]
 }  deriving Show
 
-type Accion = (Participante -> Participante)
+type Accion = (Participante -> Participante) 
 type Tactica = String 
 type Propiedad = (String,Int)
+type Nombre = String
 
 carolina :: Participante
 carolina = Participante "Carolina" 500 "Accionista" [] [pasarPorElBanco, pagarAccionistas]
@@ -20,18 +21,18 @@ manuel :: Participante
 manuel = Participante "Manuel" 500 "Oferente Singular" [] [pasarPorElBanco, enojarse]
 
 pasarPorElBanco :: Participante -> Participante
-pasarPorElBanco unparticipante = unparticipante {tacticaDeJuego = "Coprador compulsivo", cantidadDeDinero = cantidadDeDinero unparticipante + 40}
+pasarPorElBanco unparticipante = cambiarTactica "Comprador compulsivo" (cambiarDinero 40 unparticipante)
 
-gritar :: Participante -> Participante
-gritar unparticipante = unparticipante {nombre = (("AHHHH" ++).nombre) unparticipante }
+gritar :: Participante -> Participante 
+gritar unparticipante = cambiarNombre unparticipante "AHHHH"
 
 enojarse :: Participante -> Participante
-enojarse unparticipante = unparticipante {cantidadDeDinero = cantidadDeDinero unparticipante + 50, accionesDelJuego = accionesDelJuego unparticipante ++ [gritar] }
+enojarse unparticipante = agregarAccion (cambiarDinero 50 unparticipante) [gritar]
 
 pagarAccionistas :: Participante -> Participante
 pagarAccionistas  unparticipante
-    | (esAccionista.tacticaDeJuego) unparticipante = unparticipante {cantidadDeDinero = cantidadDeDinero unparticipante + 200} 
-    | otherwise = unparticipante {cantidadDeDinero = cantidadDeDinero unparticipante - 100}
+    | (esAccionista.tacticaDeJuego) unparticipante = cambiarDinero 200 unparticipante 
+    | otherwise = cambiarDinero (0-100) unparticipante
 
 esAccionista :: Tactica -> Bool
 esAccionista "Accionista" = True
@@ -43,8 +44,7 @@ esOferenteSingular _ = False
 
 subastar :: Participante -> Propiedad -> Participante
 subastar unparticipante unapropiedad
-    | (esAccionista.tacticaDeJuego) unparticipante  = unparticipante {cantidadDeDinero = cantidadDeDinero unparticipante - (snd unapropiedad), propiedadesCompradas = propiedadesCompradas unparticipante ++ [unapropiedad] }
-    | (esOferenteSingular.tacticaDeJuego) unparticipante = unparticipante {cantidadDeDinero = cantidadDeDinero unparticipante - (snd unapropiedad), propiedadesCompradas = propiedadesCompradas unparticipante ++ [unapropiedad] }
+    | (esAccionista.tacticaDeJuego) unparticipante || (esOferenteSingular.tacticaDeJuego) unparticipante  = cambiarDinero (0-(snd unapropiedad)) (agregarPropiedad unparticipante [unapropiedad])
     | otherwise = unparticipante
 
 esPropiedadBarata :: Propiedad -> Bool
@@ -57,4 +57,47 @@ contarPropideadesCaras :: Participante -> Int
 contarPropideadesCaras unparticipante = length(filter (not.esPropiedadBarata) (propiedadesCompradas unparticipante))
 
 cobrarAlquileres :: Participante -> Participante
-cobrarAlquileres unparticipante = unparticipante {cantidadDeDinero = cantidadDeDinero unparticipante + (10*(contarPropideadesBaratas unparticipante)) + (20*(contarPropideadesCaras unparticipante))}
+cobrarAlquileres unparticipante = cambiarDinero ((10*(contarPropideadesBaratas unparticipante)) + (20*(contarPropideadesCaras unparticipante))) unparticipante
+
+cambiarDinero :: Int -> Participante -> Participante
+cambiarDinero unNumero unparticipante  = unparticipante {cantidadDeDinero = cantidadDeDinero unparticipante + unNumero}
+
+cambiarTactica :: Tactica -> Participante -> Participante
+cambiarTactica unaTactica unparticipante = unparticipante {tacticaDeJuego = unaTactica }
+
+cambiarNombre :: Participante -> Nombre -> Participante
+cambiarNombre unparticipante unNombre = unparticipante {nombre = ((unNombre ++).nombre)unparticipante}
+
+agregarPropiedad :: Participante -> [Propiedad] -> Participante
+agregarPropiedad unparticipante listaDePropiedades = unparticipante {propiedadesCompradas = propiedadesCompradas unparticipante ++ listaDePropiedades}
+
+agregarAccion :: Participante -> [Accion] -> Participante
+agregarAccion unparticipante listaDeAcciones = unparticipante {accionesDelJuego = accionesDelJuego unparticipante ++ listaDeAcciones}
+
+hacerBerrinchePor :: Propiedad -> Participante -> Participante
+hacerBerrinchePor unaPropiedad unparticipante = agregarAccion (cambiarDinero 10 unparticipante) [gritar]
+
+dejarDeHacerBerrinche :: Propiedad -> Participante -> Participante
+dejarDeHacerBerrinche unaPropiedad unparticipante 
+    | elJugadorTieneLaPropiedad unaPropiedad unparticipante = borrarAccionGritar unparticipante
+    | otherwise = unparticipante
+
+esGritar :: Accion -> Bool
+esGritar gritar = False
+
+borrarAccionGritar :: Participante -> Participante
+borrarAccionGritar unparticipante  = unparticipante {accionesDelJuego = filter esGritar (accionesDelJuego unparticipante)}
+
+elJugadorTieneLaPropiedad :: Propiedad -> Participante -> Bool
+elJugadorTieneLaPropiedad unaPropiedad unparticipante = elem unaPropiedad (propiedadesCompradas unparticipante)    
+
+ultimaRonda :: Participante -> [Accion]
+ultimaRonda unparticipante = accionesDelJuego unparticipante
+
+juegoFinal :: Participante -> Participante -> Participante
+juegoFinal primerPrticipante segundoParticipante = compararDinero primerPrticipante segundoParticipante
+
+compararDinero :: Participante-> Participante -> Participante
+compararDinero primerPrticipante segundoParticipante  
+    | (cantidadDeDinero primerPrticipante) < (cantidadDeDinero segundoParticipante) = segundoParticipante
+    | (cantidadDeDinero primerPrticipante) > (cantidadDeDinero segundoParticipante) = primerPrticipante
